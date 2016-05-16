7e0ee765d4c29af886769eb307bad13312ae94bd
Tim Tyrrell <tim@tyrrell.io>
2016-05-15 19:18:49 -0500

    Show random color

--- modules/components/Color/index.js
+++ modules/components/Color/index.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { header } from './styles.css'
-
-const colorsList = require('./colors')
-
-// let color = _.random(Object.keys(colorList.colors))
-//
-// colorList[color].H
-
+import shuffle from 'lodash.shuffle'
+const colorList = require('./colors')
 
 export default React.createClass({
+  componentWillMount() {
+    const randomColor = shuffle(Object.keys(colorList))[0]
+    this.setState({
+      name: randomColor,
+      details: colorList[randomColor]
+    })
+  },
+
   render() {
+    const { name, details} = this.state
+
+    const rgb = `RGB = ${details.R}, ${details.G}, ${details.B}`
+    const hsv = `RGB = ${details.H}, ${details.S}, ${details.V}`
+
     return (
       <div>
-        {/*<h2 className={header}>{colorsList.colors}</h2>*/}
-        <h1 className={header}>Red</h1>
-        <h2 className={header}>#ED0A3F</h2>
-        <h3 className={header}>RGB = 237, 10, 63</h3>
-        <h3 className={header}>HSV = 346, 96, 93</h3>
-        <p>Produced 1903–present</p>
+        <h1 className={header}>{name}</h1>
+        <h2 className={header}>{details.Hexadecimal}</h2>
+        <h3 className={header}>{rgb}</h3>
+        <h3 className={header}>{hsv}</h3>
+        <p>{details.Notes}</p>
       </div>
     )
   }
