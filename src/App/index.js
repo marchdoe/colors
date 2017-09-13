8e5b8b3c7e2f590ec9ceee9c5872ed8b7f363a13
Doug March <dougmarch@gmail.com>
2017-09-13 15:48:50 -0400

    update and clean up

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import Color from '../Color';
-import './App.css';
+import { injectGlobal } from 'styled-components'
+import { Provider } from 'rebass'
+import Color from '../Color'
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    cursor: pointer;
+  }
+
+  *::selection,
+  *::-moz-selection {
+    background: transparent;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <Color />
+      <Provider>
+        <Color />
+      </Provider>
     );
   }
 }
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const color = {
+  gray30: '#e3e3e3',
+  gray60: '#6f6f6f'
+}
+
+export default {
+  color
+}
